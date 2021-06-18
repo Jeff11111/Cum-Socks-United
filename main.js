22239c4625fabd5d157fbf69c40c31339cd13a31
Useroth <37159550+Useroth@users.noreply.github.com>
2021-06-18 04:54:55 +0200

    Let's try this.

--- main.js
+++ main.js
@@ -1,39 +1,80 @@
-const Discord = require('discord.js');
+const fs = require("fs");
+const Discord = require("discord.js");
+
+const { prefix, token } = require("./config.json");
+
+var HornyCounter = require('./counter.js');
 
 const client = new Discord.Client();
 
-const prefix = 'h!';
-
-const fs = require('fs');
-client.msgs = require ("./HornyCounter.json");
+// Reading .js files from the "commands" folder, to dynamically execute them later.
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`)
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  if (command.name) client.commands.set(command.name, command);
 }
 
+// A collection to keep the command cooldowns in.
+const cooldowns = new Discord.Collection();
 
-client.once('ready', () => {
-    console.log('Horny Boy Counter is online, God Save Us.');
+client.once("ready", () => {
+  console.log("Horny Boy Counter is online, God Save Us.");
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+// Let's login, when ready
+client.login(token);
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if(command === 'menino'){
-        client.commands.get('menino').execute(message, args, Discord);
-     } else if (command === 'estado'){
-        client.commands.get('estado').execute(message, args, Discord);
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
 
-    }
+  //If there's nothing after the prefix, do nothing.
+  if (message.content.slice(prefix.length).trim().length === 0) {
+    return;
+  }
+
+  //Try to execute the command.
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(exitCode) {
+    console.log('Closing...');
     
-});
+    let savehornydata = JSON.stringify(HornyCounter);
+    fs.writeFileSync('./HornyCounter.json', savehornydata);
 
-client.login('ODU0MjI4MjYxNzE3Mjc4NzYw.YMg4EA.EU1Q4S1NuPly57KQpHGfSPV4ZlE');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    process.exit();
+}
 
+//do something when app is closing
+process.on('exit', exitHandler.bind(null));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null));
+process.on('SIGUSR2', exitHandler.bind(null));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null));
